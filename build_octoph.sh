9b95c5195ef7cef2e4b28ddb9016e51e5c5781b4
dmarce1 <dmarce504@gmail.com>
2019-01-16 07:59:25 -0600

    adding multiple cell directories

--- build_octoph.sh
+++ build_octoph.sh
@@ -16,16 +16,11 @@ rm -r CMakeFiles
 
 
 cmake -DCMAKE_PREFIX_PATH="$HOME/local/$1/hpx" -DCMAKE_CXX_FLAGS="-DBOOST_USE_VALGRIND" \
-      -DCMAKE_Fortran_COMPILER=gfortran \
       -DCMAKE_CXX_COMPILER=g++ \
       -DCMAKE_C_COMPILER=gcc \
       -DCMAKE_CXX_FLAGS="-DBOOST_USE_VALGRIND -L$HOME/local/boost/lib -march=native" \
       -DCMAKE_C_FLAGS="-L$HOME/local/boost/lib" \
       -DCMAKE_BUILD_TYPE=$1                                                                                                                            \
-      -DHDF5_LIBRARY=$HOME/local/hdf5/lib/libhdf5.a \
-      -DHDF5_INCLUDE_DIR=$HOME/local/hdf5/include \
-      -DSilo_LIBRARY=$HOME/local/silo/lib/libsiloh5.a \
-      -DSilo_INCLUDE_DIR=$HOME/local/silo/include \
       ..
 
 
--- src/cell/cell_directory.cpp
+++ src/cell/cell_directory.cpp
@@ -1,64 +1,55 @@
 #include "cell/cell_directory.hpp"
 
-cell_directory::directory_type cell_directory::dir_;
-cell_directory::mutex_type cell_directory::mtx_;
+std::vector<cell_directory::directory_type> cell_directory::dir_;
+std::vector<cell_directory::mutex_type> cell_directory::mtx_;
 
-HPX_PLAIN_ACTION(cell_directory::add_entry, add_entry_action);
-HPX_PLAIN_ACTION(cell_directory::get_entry, get_entry_action);
-HPX_PLAIN_ACTION(cell_directory::del_entry, del_entry_action);
+HPX_PLAIN_ACTION(cell_directory::add_entry_with_id, add_entry_with_id_action);
+HPX_PLAIN_ACTION(cell_directory::get_entry_with_id, get_entry_with_id_action);
+HPX_PLAIN_ACTION(cell_directory::del_entry_with_id, del_entry_with_id_action);
 
-hpx::future<void> cell_directory::add_entry(const cell_location& loc,
+hpx::future<void> cell_directory::add_entry(int i, const cell_location& loc,
 		const cell_client& client) {
 	static const auto localities = hpx::find_all_localities();
 	const auto id = loc.locality_id();
 	hpx::future<void> fut;
 	if (id == hpx::get_locality_id()) {
 		{
-			std::lock_guard<mutex_type> lock(mtx_);
-			dir_[loc] = client;
+			std::lock_guard<mutex_type> lock(mtx_[i]);
+			dir_[i][loc] = client;
 		}
 		fut = hpx::make_ready_future<void>();
 	} else {
-		fut = hpx::async<add_entry_action>(localities[id], loc, client);
+		fut = hpx::async<add_entry_with_id_action>(localities[id], i, loc, client);
 	}
 	return std::move(fut);
 }
 
-hpx::future<cell_client> cell_directory::get_entry(const cell_location& loc) {
+hpx::future<cell_client> cell_directory::get_entry_with_id(int i, const cell_location& loc) {
 	static const auto localities = hpx::find_all_localities();
 	const auto id = loc.locality_id();
 	hpx::future<cell_client> fut;
 	if (id == hpx::get_locality_id()) {
-		std::lock_guard<mutex_type> lock(mtx_);
-		fut = hpx::make_ready_future(dir_[loc]);
+		std::lock_guard<mutex_type> lock(mtx_[i]);
+		fut = hpx::make_ready_future(dir_[i][loc]);
 	} else {
-		fut = hpx::async<get_entry_action>(localities[id], loc);
+		fut = hpx::async<get_entry_with_id_action>(localities[id], i, loc);
 	}
 	return std::move(fut);
 }
 
-hpx::future<void> cell_directory::del_entry(const cell_location& loc) {
+hpx::future<void> cell_directory::del_entry_with_id(int i, const cell_location& loc) {
 	static const auto localities = hpx::find_all_localities();
 	hpx::future<void> fut;
 	const auto id = loc.locality_id();
 	if (id == hpx::get_locality_id()) {
 		{
-			std::lock_guard<mutex_type> lock(mtx_);
-			dir_.erase(loc);
+			std::lock_guard<mutex_type> lock(mtx_[i]);
+			dir_[i].erase(loc);
 		}
 		fut = hpx::make_ready_future<void>();
 	} else {
-		fut = hpx::async<del_entry_action>(localities[id], loc);
+		fut = hpx::async<del_entry_with_id_action>(localities[i], loc);
 	}
 	return std::move(fut);
 }
-
-static cell_directory cell_directory_instance_;
-
-cell_directory::~cell_directory() {
-	std::lock_guard<mutex_type> lock(mtx_);
-	dir_.clear();
-}
-
-
 

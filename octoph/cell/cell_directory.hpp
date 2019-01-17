76032b3fe41b1cbf19fe57ff75fab72bb33942dc
dmarce1 <dmarce504@gmail.com>
2019-01-17 07:48:52 -0600

    Working on cell directory

--- octoph/cell/cell_directory.hpp
+++ octoph/cell/cell_directory.hpp
@@ -11,25 +11,8 @@
 #include "cell/cell_client.hpp"
 #include "cell/cell_location.hpp"
 
-#include <hpx/lcos/local/mutex.hpp>
 
-#include <unordered_map>
-#include <vector>
 
-class cell_directory {
-private:
-	using directory_type = std::unordered_map<cell_location,cell_client,cell_location::hash>;
-	using mutex_type = hpx::lcos::local::mutex;
-	static std::vector<directory_type> dir_;
-	static std::vector<mutex_type> mtx_;
-	int this_id;
-	static hpx::future<void> add_entry_with_id(int, const cell_location&, const cell_client&);
-	static hpx::future<cell_client> get_entry_with_id(int, const cell_location&);
-	static hpx::future<void> del_entry_with_id(int, const cell_location&);
-public:
-	hpx::future<void> add_entry(const cell_location&, const cell_client&);
-	hpx::future<cell_client> get_entry(const cell_location&);
-	hpx::future<void> del_entry(const cell_location&);
-};
+
 
 #endif /* OCTOPH_CELL_CELL_DIRECTORY_HPP_ */
--- src/cell/cell_directory.cpp
+++ src/cell/cell_directory.cpp
@@ -1,55 +1,84 @@
 #include "cell/cell_directory.hpp"
 
-std::vector<cell_directory::directory_type> cell_directory::dir_;
-std::vector<cell_directory::mutex_type> cell_directory::mtx_;
+#include <hpx/lcos/local/mutex.hpp>
 
-HPX_PLAIN_ACTION(cell_directory::add_entry_with_id, add_entry_with_id_action);
-HPX_PLAIN_ACTION(cell_directory::get_entry_with_id, get_entry_with_id_action);
-HPX_PLAIN_ACTION(cell_directory::del_entry_with_id, del_entry_with_id_action);
+#include <array>
+#include <unordered_map>
 
-hpx::future<void> cell_directory::add_entry(int i, const cell_location& loc,
-		const cell_client& client) {
+using directory_type = std::unordered_map<cell_location,cell_client,cell_location::hash>;
+using mutex_type = hpx::lcos::local::mutex;
+
+static mutex_type mtx_;
+
+static std::unordered_map<int, std::shared_ptr<directory_type>> dirs_;
+static std::atomic<int> next_handle_ = 0;
+
+static hpx::future<int> gen_handle();
+static hpx::future<void> add(int, const cell_location&, cell_client);
+static hpx::future<void> del(int, const cell_location&);
+static hpx::future<cell_client> get(int, const cell_location&);
+
+HPX_PLAIN_ACTION(gen_handle, gen_handle_action);
+HPX_PLAIN_ACTION(add, add_action);
+HPX_PLAIN_ACTION(del, del_action);
+HPX_PLAIN_ACTION(get, get_action);
+
+static hpx::future<void> add(int dir_index, const cell_location& location, cell_client client) {
 	static const auto localities = hpx::find_all_localities();
-	const auto id = loc.locality_id();
+	static const auto this_id = hpx::get_locality_id();
 	hpx::future<void> fut;
-	if (id == hpx::get_locality_id()) {
-		{
-			std::lock_guard<mutex_type> lock(mtx_[i]);
-			dir_[i][loc] = client;
+	const auto loc_id = location.locality_id();
+	if( this_id == loc_id ) {
+		std::lock_guard<mutex_type> lock(mtx_);
+		if( dirs_[dir_index] == nullptr ) {
+			dirs_[dir_index] = std::make_shared<directory_type>();
 		}
-		fut = hpx::make_ready_future<void>();
+		(*dirs_[dir_index])[location] = client;
+		fut = hpx::make_ready_future();
 	} else {
-		fut = hpx::async<add_entry_with_id_action>(localities[id], i, loc, client);
+		fut = hpx::async<add_action>(localities[loc_id], dir_index, location, client);
 	}
 	return std::move(fut);
 }
 
-hpx::future<cell_client> cell_directory::get_entry_with_id(int i, const cell_location& loc) {
+static hpx::future<cell_client> get(int dir_index, const cell_location& location) {
 	static const auto localities = hpx::find_all_localities();
-	const auto id = loc.locality_id();
+	static const auto this_id = hpx::get_locality_id();
 	hpx::future<cell_client> fut;
-	if (id == hpx::get_locality_id()) {
-		std::lock_guard<mutex_type> lock(mtx_[i]);
-		fut = hpx::make_ready_future(dir_[i][loc]);
+	const auto loc_id = location.locality_id();
+	if( this_id == loc_id ) {
+		std::lock_guard<mutex_type> lock(mtx_);
+		fut = hpx::make_ready_future((*dirs_[dir_index])[location]);
 	} else {
-		fut = hpx::async<get_entry_with_id_action>(localities[id], i, loc);
+		fut = hpx::async<get_action>(localities[loc_id], dir_index, location);
 	}
 	return std::move(fut);
 }
 
-hpx::future<void> cell_directory::del_entry_with_id(int i, const cell_location& loc) {
+static hpx::future<void> del(int dir_index, const cell_location& location) {
 	static const auto localities = hpx::find_all_localities();
+	static const auto this_id = hpx::get_locality_id();
 	hpx::future<void> fut;
-	const auto id = loc.locality_id();
-	if (id == hpx::get_locality_id()) {
-		{
-			std::lock_guard<mutex_type> lock(mtx_[i]);
-			dir_[i].erase(loc);
-		}
-		fut = hpx::make_ready_future<void>();
+	const auto loc_id = location.locality_id();
+	if( this_id == loc_id ) {
+		std::lock_guard<mutex_type> lock(mtx_);
+		(*dirs_[dir_index]).erase(location);
 	} else {
-		fut = hpx::async<del_entry_with_id_action>(localities[i], loc);
+		fut = hpx::async<del_action>(localities[loc_id], dir_index, location);
 	}
 	return std::move(fut);
 }
+
+static hpx::future<int> gen_handle() {
+	static const auto localities = hpx::find_all_localities();
+	static const auto this_id = hpx::get_locality_id();
+	if (this_id == 0) {
+		return hpx::make_ready_future(next_handle_++);
+	} else {
+		return hpx::async<gen_handle_action>(localities[0]);
+	}
+}
+
+
+
 

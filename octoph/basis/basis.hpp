02f372f500a9bdfb1241d1bd20cf715bb6b5197f
dmarce1 <dmarce504@gmail.com>
2019-01-17 07:49:21 -0600

    Added files for basis

---
+++ octoph/basis/basis.hpp
@@ -0,0 +1,14 @@
+/*
+ * basis.hpp
+ *
+ *  Created on: Jan 16, 2019
+ *      Author: dmarce1
+ */
+
+#ifndef OCTOPH_BASIS_BASIS_HPP_
+#define OCTOPH_BASIS_BASIS_HPP_
+
+#include "basis/legendre.hpp"
+
+
+#endif /* OCTOPH_BASIS_BASIS_HPP_ */
---
+++ src/basis/basis.cpp
@@ -0,0 +1,10 @@
+/*
+ * basis.cpp
+ *
+ *  Created on: Jan 16, 2019
+ *      Author: dmarce1
+ */
+
+#include "basis/basis.hpp"
+
+

71cef423107e9c81cf895f0fa9d1651c4b18233f
dmarce1 <dmarce504@gmail.com>
2019-02-08 16:20:20 -0600

    integer list splitting and concatenation

--- src/math/math.cpp
+++ src/math/math.cpp
@@ -2,7 +2,120 @@
 #include <octoph/math/containers.hpp>
 #include <hpx/hpx_init.hpp>
 
+#include <utility>
+#include <cstdint>
+#include <cmath>
+#include <array>
+#include <tuple>
+#include <utility>
 
+template<int...>
+struct multiindex;
+
+template<int I, int J, int ...K>
+struct multiindex<I, J, K...> : public std::integer_sequence<int, I, J, K...> {
+	using base_type = std::integer_sequence<int, I, J, K...>;
+
+	template<int L>
+	static int get() {
+		if constexpr (L == 0) {
+			return I;
+		} else {
+			return multiindex<J, K...>::template get<L - 1>();
+		}
+	}
+
+	template<int, int...>
+	struct split_help;
+
+	template<int L, int M, int ...N>
+	struct split_help<L,M,N...> {
+		template<int ...O>
+		using type_help = typename std::conditional<
+		L!=0,
+		typename split_help<L-1,N...>::template type_help<O...,M>,
+		std::pair<multiindex<O...>, multiindex<M,N...>>
+		>::type;
+		using type = type_help<>;
+	};
+
+	template<int L>
+	struct split_help<L> {
+		template<int ...O>
+		using type_help = std::pair<multiindex<O...>, multiindex<>>;
+		using type = type_help<>;
+	};
+
+	template<int L>
+	using split = typename split_help<L,I,J,K...>::type;
+
+	static void print(bool first = true) {
+		if( first ) {
+			printf( "\n");
+		}
+		printf( "%i ", I);
+		multiindex<J,K...>::print(false);
+		if( first ) {
+			printf( "\n");
+		}
+	}
+
+	multiindex() = default;
+	multiindex(const multiindex&) = default;
+	multiindex& operator=(const multiindex&) = default;
+
+};
+
+template<int I>
+struct multiindex<I> : public std::integer_sequence<int, I> {
+	using base_type = std::integer_sequence<int, I>;
+
+	template<int L>
+	static int get() {
+		return I;
+	}
+
+	template<class L>
+	using split = multiindex;
+
+	static void print(bool first = true) {
+		if (first) {
+			printf("\n");
+		}
+		printf("%i ", I);
+		if (first) {
+			printf("\n");
+		}
+	}
+
+	multiindex() = default;
+	multiindex(const multiindex&) = default;
+	multiindex& operator=(const multiindex&) = default;
+
+};
+
+template<>
+struct multiindex<> : public std::integer_sequence<int> {
+	using base_type = std::integer_sequence<int>;
+	static void print(bool first = true) {
+		if (first) {
+			printf("\n");
+		}
+		if (first) {
+			printf("\n");
+		}
+	}
+
+	multiindex() = default;
+	multiindex(const multiindex&) = default;
+	multiindex& operator=(const multiindex&) = default;
+};
+
+template<int ...I, int ...J>
+auto multiindex_cat(const multiindex<I...>& a, const multiindex<J...>& b) {
+	multiindex<I..., J...> i;
+	return i;
+}
 
 #ifdef TEST_MATH
 
@@ -11,12 +124,17 @@ int main(int argc, char* argv[]) {
 }
 
 int hpx_main(int argc, char* argv[]) {
-	using namespace math;
-	math_array<double,4000> t1;
-	math_array<double,4000> t2;
-	const auto t3 = (t1 + t2) * 5.0;
-	const auto t5 = (t3 * t2) * t1;
-	math_array<double,4000> t4 = t5;
+
+	multiindex<1,2,3,4,5> test;
+	test.print();
+
+	using split = typename decltype(test)::split<3>;
+	split().first.print();
+	split().second.print();
+
+	using cat_type = decltype(multiindex_cat( split().first, split().second));
+	cat_type::print();
+
 	return hpx::finalize();
 
 }
--- src/math/tensor.cpp
+++ src/math/tensor.cpp
@@ -10,6 +10,7 @@
 #include <cmath>
 #include <array>
 #include <tuple>
+#include <utility>
 
 template<int...>
 struct multiindex;
@@ -27,25 +28,17 @@ struct multiindex<I, J, K...> : public std::integer_sequence<int, I, J, K...> {
 		}
 	}
 
-	template<int, int...>
-	struct swap_with_top_type;
-
 	template<int L, int M, int ...N>
-	struct swap_with_top_type<L,M,N...> {
-		using type = typename std::conditional<
-		L==0,
-		multiindex<I, N..., M, J, K...>,
-		typename multiindex<J, K...>::template swap_with_top_type<L - 1, M, N..., I>::type
+	struct split {
+	private:
+		template<int ...O>
+		using type_help = typename std::conditional<
+				L!=0,
+				typename split<L - 1,N...>::template type<O...,M>,
+				std::pair<multiindex<O...>, multiindex<M,N...>>
 		>::type;
-	};
-
-	template<int L>
-	struct swap_with_top_type<L> {
-		using type = typename std::conditional<
-		L==0,
-		multiindex<I, J, K...>,
-		typename multiindex<J, K...>::template swap_with_top_type<L - 1, I>::type
-		>::type;
+	public:
+		using type = type_help<>;
 	};
 
 };
@@ -59,126 +52,7 @@ struct multiindex<I> : public std::integer_sequence<int, I> {
 		return I;
 	}
 
-	template<int, int...>
-	struct swap_with_top_type;
-
-	template<int L, int M, int ...N>
-	struct swap_with_top_type<L,M,N...> {
-		using type = multiindex<I, N..., M>;
-	};
-
-	template<int L>
-	struct swap_with_top_type<L> {
-		using type = multiindex;
-	};
-
 };
 
-//
-//template<bool, int...>
-//struct static_multiindex;
-//
-//template<int I, int ...J>
-//struct static_multiindex<false, I, J...> : public std::integer_sequence<int, I, J...> {
-//	static constexpr int next_index = I;
-//	using remove_next_index_type = static_multiindex<false,J...>;
-//};
-//
-//template<int I>
-//struct static_multiindex<false, I> : public std::integer_sequence<int, I> {
-//	static constexpr int next_index = I;
-//};
-//
-//template<int I, int J, int ...K>
-//struct static_multiindex<true, I, J, K...> : public std::integer_sequence<int, I, J, K...> {
-//	static constexpr int next_index =
-//			I<J ? static_multiindex<true, J, K...>::next_index : static_multiindex < true, I, K...>::next_index;
-//	using remove_next_index_type = typename std::conditional<
-//	I < J,
-//	typename static_multiindex<true,J, K...>::remove_next_index_type,
-//	typename static_multiindex<true,I, K...>::remove_next_index_type
-//	>::type;
-//};
-//
-//template<int I, int J>
-//struct static_multiindex<true, I, J> : public std::integer_sequence<int, I, J> {
-//	static constexpr int next_index = I < J ? J : I;
-//	using remove_next_index_type = typename std::conditional<
-//	I < J,
-//	static_multiindex<true,J>,
-//	static_multiindex<true,I>
-//	>::type;
-//};
-//
-//template<int I>
-//struct static_multiindex<true, I> : public std::integer_sequence<int, I> {
-//	static constexpr int next_index = I;
-//};
-//
-//template<class Type, int Rank, int Dim, bool Symmetric = false>
-//struct tensor {
-//
-//	template<class I>
-//	Type get() const {
-//		return std::get < I::next_index > (tensor_tuple_).template get<typename I::remove_next_index_type>();
-//	}
-//
-//	template<int I, int ...J>
-//	Type get() const {
-//		return get<static_multiindex<Symmetric, I, J...>>();
-//	}
-//
-//private:
-//	template<int I = Dim - 1>
-//	static auto tuple_type_proto() {
-//		constexpr int NextDim = Symmetric ? I + 1 : Dim;
-//		std::tuple<tensor<Type, Rank - 1, NextDim>> this_tuple;
-//		if constexpr (I == 0) {
-//			return this_tuple;
-//		} else {
-//			auto rest_tuple = tuple_type_proto<I - 1>();
-//			return std::tuple_cat(this_tuple, rest_tuple);
-//		}
-//	}
-//
-//	using tuple_type = decltype(tuple_type_proto());
-//
-//	tuple_type tensor_tuple_;
-//
-//};
-//
-//template<class Type, int Dim>
-//struct tensor<Type, 1, Dim> {
-//
-//	template<class I>
-//	Type get() const {
-//		return std::get < I::next_index > (tensor_tuple_);
-//	}
-//
-//	template<int I>
-//	Type get() const {
-//		return get<static_multiindex<I>>();
-//	}
-//
-//private:
-//	template<int I = Dim - 1>
-//	static auto tuple_type_proto() {
-//		std::tuple<Type> this_tuple;
-//		if constexpr (I == 0) {
-//			return this_tuple;
-//		} else {
-//			auto rest_tuple = tuple_type_proto<I - 1>();
-//			return std::tuple_cat(this_tuple, rest_tuple);
-//		}
-//	}
-//
-//	using tuple_type = decltype(tuple_type_proto());
-//
-//	tuple_type tensor_tuple_;
-//
-//};
-
 void test() {
-//	tensor<double, 2, 3, false> test;
-//	test.get<0, 0>();
 }
